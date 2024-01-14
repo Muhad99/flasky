8040280ac3fd51fbe1d41f1ba4a6f98d43640224
Muhad99 <145128166+Muhad99@users.noreply.github.com>
2024-01-14 23:35:51 +0100

    add it

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 from ensurepip import bootstrap
-import os
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask import request
 from flask import make_response
@@ -9,19 +8,13 @@ from flask import abort
 from flask_moment import Moment 
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, SubmitField
+from wtforms import StringField, SubmitField,validators
 from wtforms.validators import DataRequired 
-from flask_sqlalchemy import SQLAlchemy
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -59,59 +52,27 @@ moment = Moment(app)
 # def index():
 #     return render_template('index.html',current_time=datetime.utcnow())
 
-class NameForm(FlaskForm):
+class NameForm():
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     name = None
-#     form = NameForm()
-#     if form.validate_on_submit():
-#         name = form.name.data
-#         form.name.data = ''
-#     return render_template('index.html', form=form, name=name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    formi = NameForm()
+    if FlaskForm.validate_on_submit():
+        name = FlaskForm.name.data
+        FlaskForm.name.data = ''
+        return render_template('index.html', form=formi, name=name)
 
-# @app.route('/', methods=['GET','POST'])
-# def index():
-#     form = NameForm()
-#     if form.validate():
-#         session['name'] = form.name.data
-#         return redirect(url_for('index'))
-#     return render_template('index.html', form=form, name=session.get('name'))
-    
-class Role(db.Model):
-   __tablename__ = 'roles'
-   id = db.Column(db.Integer, primary_key=True)
-   name = db.Column(db.String(64), unique=True) 
-
-   def __repr__(self):
-      return '<Role %r>' % self.name
-class User(db.Model):
-   __tablename__ = 'users'
-   id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String(64), unique=True, index=True)
-
-   def __repr__(self):
-      return '<User %r>' % self.username
-
-class Role(db.Model):
-   users = db.relationship('User', backref='role')
-class User(db.Model):
-   role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     form = NameForm()
-#     if form.validate_on_submit():
-#         old_name = session.get('name')
-#         if old_name is not None and old_name != form.name.data:
-#            flash('Looks like you have changed your name!')
-#         session['name'] = form.name.data
-#         form.name.data = ''
-#         return redirect(url_for('index'))
-#     return render_template('index.html',form = form, name = session.get('name'))
+@app.route('/', methods=['GET','POST'])
+def index():
+    from = NameFrom()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 
